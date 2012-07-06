9d2b24279c2e82529accda9e387927ed296758f0
Frederik Hahne <frederik.hahne@googlemail.com>
2012-07-06 14:32:08 +0200

    Added License Header
    Added the missing GPL License Header.

--- mensa-upb/src/de/atomfrede/android/mensa/data/Quicklunch.java
+++ mensa-upb/src/de/atomfrede/android/mensa/data/Quicklunch.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2012 Frederik Hahne
+ *  
+ *  This file is part of Mensa UPB.
+ *
+ *  Mensa UPB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Mensa UPB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Mensa UPB.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.atomfrede.android.mensa.data;
 
 public class Quicklunch {
