39ef21983ef357f48a249b68d1ec3a0a6ee8119e
Frederik Hahne <frederik.hahne@googlemail.com>
2012-11-01 12:16:20 +0100

    Added android annotations.

---
+++ mensa-upb/src/main/java/de/atomfrede/android/mensa/upb/common/LocationFragment.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2012 Frederik Hahne
+ *  
+ *  This file is part of Mensa UPB.
+ *
+ *  Mensa UPB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Mensa UPB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Mensa UPB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.atomfrede.android.mensa.upb.common;
+
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+import de.atomfrede.android.mensa.R;
+
+@EFragment(R.layout.fragment_location_list)
+public class LocationFragment extends SherlockFragment {
+
+	@ViewById(R.id.location_list)
+	public ListView locationList;
+}
